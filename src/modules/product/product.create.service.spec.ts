e2fa91709c584ac69384973cf279e4d8260e0b2e
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-05-11 18:29:20 -0300

    feat: test product already  exists

--- src/modules/product/product.create.service.spec.ts
+++ src/modules/product/product.create.service.spec.ts
@@ -1,11 +1,18 @@
 import { CreateProductService } from "./product.create.service";
 import { ProductInMemoryRepository } from "./repositories/ProductInMemoryRepository";
 
+let createProductService: CreateProductService;
+let productInMemoryRepository: ProductInMemoryRepository;
+
 describe('Create Product', () => {
 
+  beforeAll(() => {
+    productInMemoryRepository = new ProductInMemoryRepository();
+    createProductService = new CreateProductService(productInMemoryRepository);   
+  })
+
   it('should be possible to create a product', async () => {
-    const productInMemoryRepository = new ProductInMemoryRepository();
-    const createService = new CreateProductService(productInMemoryRepository);
+
 
     const product = {
       name: 'Product_test',
@@ -13,10 +20,23 @@ describe('Create Product', () => {
       price: 100
     }
 
-    const result = await createService.execute(product)
+    const result = await createProductService.execute(product)
 
     expect(result).toHaveProperty('id')
   })
 
+  it('should not be possible to create a product with the bar_code that already exists', async  () => {
+
+    const product = {
+      name: 'Product_test',
+      bar_code: 'bar_code_test_2',
+      price: 100
+    }
+
+    await createProductService.execute(product)
+    await expect(createProductService.execute(product)).rejects.toEqual(new Error('Produto já cadastrado'));
+
+    
+  })
 
 })
